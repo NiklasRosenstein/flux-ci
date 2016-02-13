d85f56a8509cce8e988ff746157236f331ed9ad9
Niklas Rosenstein <rosensteinniklas@gmail.com>
2016-02-13 18:29:05 +0100

    update comments in flux_config.py, add flux_secret.py and requirements.txt, update executable permissions

--- flux_config.py
+++ flux_config.py
@@ -7,9 +7,15 @@ port = 4042
 debug = True
 
 # Full path to the SSH identity file, or None to let SSH decide.
+# This option can also be overwritten on a per-repository basis.
 ssh_identity_file = None
 
-# Dictionary to configure accepted repositories. Example:
+# Dictionary to configure accepted repositories. The secret is
+# either provided by the webhook registrar or it allows you to
+# specify a secret manually. In case of the latter, you can
+# generate a new secret with the `flux_secret.py` script.
+#
+# Example configuration:
 #
 #   'gildarts/awesome-app': {
 #     'secret': 'your-secret-key',
--- flux_run.py
+++ flux_run.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 # Copyright (C) 2016  Niklas Rosenstein
 # All rights reserved.
 
