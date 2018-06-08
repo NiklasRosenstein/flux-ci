c793c364ae65c6d7896a26f62c6174a21ad42b03
Niklas Rosenstein <rosensteinniklas@gmail.com>
2018-06-08 14:02:57 +0200

    update Dockerfile and .dockerignore, update flux/__init__.py

--- flux/__init__.py
+++ flux/__init__.py
@@ -17,21 +17,9 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
-'''
-Flux -- Lightweight private build server
-========================================
-
-Flux is a simple, Flask_ based build server that responds to Git
-push events as triggered by GitHub, GitLab, BitBucket or Gogs.
-While it is cross-platform and can on Windows, Linux and Mac OS,
-it does not provide any kind of virtualization or container
-management.
-
-.. _Flask: http://flask.pocoo.org/
-'''
 
 __author__ = 'Niklas Rosenstein <rosensteinniklas@gmail.com>'
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 import flask
 import os
